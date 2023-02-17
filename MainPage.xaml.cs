533b6faf67716244d6a3b13756f3845070a26df8
Somgur1 <Vincedewit@live.nl>
2023-02-17 16:02:46 +0100

    Added Tamagochi class

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using Microsoft.Maui.Layouts;
+using System;
 using System.Diagnostics;
 using System.Threading;
 using System.Timers;
+using static Tamagotchi.TamaClass;
 
 namespace Tamagotchi;
 
@@ -11,9 +13,14 @@ public partial class MainPage : ContentPage
     Random rnd = new Random();
     IDispatcherTimer timer;
     Stopwatch stopwatch = new Stopwatch();
-	bool is_shiny = false;
 	int timertext = 0;
 	bool imageChangeOnce = false;
+	TamaClass Chimchar = new TamaClass("Chimchar");
+
+	
+
+	
+
 
     //To do:
     //If time is above x: evolve
@@ -24,9 +31,8 @@ public partial class MainPage : ContentPage
 		int random_number = rnd.Next(1, 101);
 		if(random_number == 1)
 		{
-			is_shiny = true;
+            Chimchar.is_shiny = true;
 		}
-		
 
         timer = Dispatcher.CreateTimer();
         timer.Interval = TimeSpan.FromSeconds(1);
@@ -35,51 +41,51 @@ public partial class MainPage : ContentPage
             
             TimerText.Text = timertext.ToString();
 
-			//health_bar.Progress = health_bar.Progress - 0.1; //for testing
+			//health_bar.Progress = Chimchar.subtractHealth(0.1); //for testing
 
 			if(happy_bar.Progress == 0)
 			{
-				health_bar.Progress = health_bar.Progress - 0.1;
+				health_bar.Progress = Chimchar.subtractHealth(0.1);
 			}
             if (hunger_bar.Progress == 0)
             {
-                health_bar.Progress = health_bar.Progress - 0.1;
+                health_bar.Progress = Chimchar.subtractHealth(0.1);
             }
-
             if (timertext % 5 == 0 && timertext != 0)
 			{
-				happy_bar.Progress = happy_bar.Progress - 0.1;
-				hunger_bar.Progress = hunger_bar.Progress - 0.1;
+				hunger_bar.Progress = Chimchar.subtractHunger(0.1);
+				happy_bar.Progress = Chimchar.subtractHappy(0.1);
 			}
 
             if (health_bar.Progress <= 0.5) 
 			{
 				if (!imageChangeOnce)
 				{
-					if (is_shiny)
+					if (Chimchar.is_shiny)
 					{
-						tamagotchi_image.Source = "shiny_chimchar_sad.png";
+                        tamagotchi_image.Source = Chimchar.SetImage("shiny_chimchar_sad.png"); 
 					}
 					else
 					{
-						tamagotchi_image.Source = "chimchar_sad.png";
+                        tamagotchi_image.Source = Chimchar.SetImage("chimchar_sad.png");
 					}
-					imageChangeOnce = true;
+                    imageChangeOnce = true;
 				}
                
             }
 
 			if (health_bar.Progress == 0)
 			{
-				if (is_shiny)
+				if (Chimchar.is_shiny)
 				{
-					tamagotchi_image.Source = "shiny_dead_chimchar.png";
+					tamagotchi_image.Source = Chimchar.SetImage("shiny_dead_chimchar.png");
 				}
 				else
 				{
-					tamagotchi_image.Source = "dead_chimchar.png";
+                    tamagotchi_image.Source = Chimchar.SetImage("dead_chimchar.png");
 				}
-				resetbtn.Opacity = 1;
+                //tamagotchi_image.Source = Chimchar.image;
+                resetbtn.Opacity = 1;
 				pet_stats.IsVisible = false;
 				give_buttons.IsVisible = false;
 				resetbtn.IsVisible = true;
@@ -93,32 +99,38 @@ public partial class MainPage : ContentPage
 	
 	private void GiveFood(object sender, EventArgs e)
 	{
-		hunger_bar.Progress = hunger_bar.Progress + 0.05;
-	}
+		Chimchar.hunger = Chimchar.hunger + 0.05;
+        hunger_bar.Progress = Chimchar.hunger;
+    }
 
     private void GiveLove(object sender, EventArgs e)
     {
-        happy_bar.Progress = happy_bar.Progress + 0.05;
+		Chimchar.happy = Chimchar.happy + 0.05;
+        happy_bar.Progress = Chimchar.happy;
     }
 
     private void resetcount(object sender, EventArgs e)
 	{
-		is_shiny = false;
+		Chimchar.is_shiny = false;
 		count = 0;
-        //CounterBtn.Text = "Click me";
-		health_bar.Progress = 1;
-		tamagotchi_image.Source = "egg.png";
+		//CounterBtn.Text = "Click me";
+		Chimchar.health = 1;
+		health_bar.Progress = Chimchar.health;
+		tamagotchi_image.Source = Chimchar.SetImage("egg.png");
 		tamagotchi_image.IsEnabled = true;
 		timertext = 0;
-		happy_bar.Progress = 1;
-		hunger_bar.Progress = 1;
-		health_bar.Progress = 1;
+		Chimchar.happy = 1;
+		happy_bar.Progress = Chimchar.happy;
+		Chimchar.hunger = 1;
+		hunger_bar.Progress = Chimchar.hunger;
+		Chimchar.health = 1;
+		health_bar.Progress = Chimchar.health;
 		pet_stats.IsVisible = true;
 		resetbtn.IsVisible = true;
         int random_number = rnd.Next(1, 101);
         if (random_number == 1)
         {
-            is_shiny = true;
+            Chimchar.is_shiny = true;
         }
     }
 
@@ -134,7 +146,7 @@ private void OnCounterClicked(object sender, EventArgs e)
 
 		if(count == 10)
 		{
-			if(is_shiny)
+			if(Chimchar.is_shiny)
 			{
 				tamagotchi_image.Source = "shiny_chimchar.png";
             }
---
+++ TamaClass.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tamagotchi
+{
+    public class TamaClass
+    {
+        public string name;
+        public string image;
+        public double health = 1;
+        public double hunger = 1;
+        public double happy = 1;
+        public bool is_shiny = false;
+
+
+
+        public TamaClass(String name)
+        {
+            this.name = name;
+        }
+
+        public string SetImage(String image)
+        {
+            this.image = image;
+            return this.image;
+        }
+
+
+
+        // Property 2
+
+        public double  subtractHunger(double by)
+        {
+            this.hunger = this.hunger - by;
+
+            return this.hunger;
+        }
+        public double subtractHealth(double by)
+        {
+            this.health = this.health - by;
+
+            return this.health;
+
+        }
+        public double subtractHappy(double by)
+        {
+            this.happy = this.happy - by;
+
+            return this.happy;
+        }
+        public double GetHealth()
+        {
+            return health;
+        }
+        public String GetName()
+        {
+            return name;
+        }
+        public string Name { get; }
+
+        // Property 3
+        public double GetHunger()
+        {
+            return hunger;
+        }
+
+        // Property 4
+        public double GetHappy()
+        {
+            return happy;
+        }
+
+
+    }
+}
